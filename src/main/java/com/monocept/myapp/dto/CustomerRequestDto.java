b93e23d347e215c9a278e8b407a4fc58e2dc02be
valletivarish <varishvaleti52@gmail.com>
2024-09-03 20:26:19 +0530

    started customer

---
+++ src/main/java/com/monocept/myapp/dto/CustomerRequestDto.java
@@ -0,0 +1,27 @@
+package com.monocept.myapp.dto;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class CustomerRequestDto {
+	private long customerId;
+
+	private String firstName;
+
+	private String lastName;
+
+	private LocalDate dateOfBirth;
+
+	private String phoneNumber;
+
+	private String username;
+	private String email;
+	private String password;
+	private String houseNo;
+	private String apartment;
+	private int pincode;
+	private long stateId;
+	private long cityId;
+}
