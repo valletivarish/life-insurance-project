2afd4789f76906fd13e075e9ce34b1ede3c2b6fd
valletivarish <varishvaleti52@gmail.com>
2024-09-05 13:35:39 +0530

    added stripe payment gateway

--- src/main/java/com/monocept/myapp/dto/PolicyAccountRequestDto.java
+++ src/main/java/com/monocept/myapp/dto/PolicyAccountRequestDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 @Data
 public class PolicyAccountRequestDto {
+	private String stripeToken; 
 	private Long insuranceSchemeId;
 	private Long agentId;
 	private PremiumType premiumType;
---
+++ src/main/java/com/monocept/myapp/dto/StripeTokenDto.java
@@ -0,0 +1,16 @@
+//package com.monocept.myapp.dto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class StripeTokenDto {
+//
+//    private String cardNumber;
+//    private String expMonth;
+//    private String expYear;
+//    private String cvc;
+//    private String token;
+//    private String username;
+//    private boolean success;
+//}
+//
